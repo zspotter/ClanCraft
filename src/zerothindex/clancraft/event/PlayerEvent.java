56c822fd00ddf75a41d656d3c841368ef637307d
Zachary Potter <zerothindex@gmail.com>
2012-03-06 18:34:24 -0500

    Added chat tags

--- src/zerothindex/clancraft/event/PlayerEvent.java
+++ src/zerothindex/clancraft/event/PlayerEvent.java
@@ -6,10 +6,13 @@ import zerothindex.clancraft.clan.ClanPlayer;
 public class PlayerEvent {
 
 	public void onPlayerChat(ClanPlayer player, String msg) {
+		String name = (player.getClan() != null)? player.getClan().getName()+" " : "";
+		name += player.getName();
+		
 		if (player.getChatMode() == ClanPlayer.CHAT_CLAN) {
 			if (player.getClan() != null) {
-				player.getClan().messageClan("<"+player.getName()+"> (clan) "+msg);
-				ClanPlugin.getInstance().log("<"+player.getName()+"> (clan) "+msg);
+				player.getClan().messageClan("<"+name+"> (clan) "+msg);
+				ClanPlugin.getInstance().log("<"+name+"> (clan) "+msg);
 				return;
 			} else {
 				player.setChatMode(ClanPlayer.CHAT_PUBLIC);
@@ -17,15 +20,15 @@ public class PlayerEvent {
 		}
 		if (player.getChatMode() == ClanPlayer.CHAT_ALLY) {
 			if (player.getClan() != null) {
-				player.getClan().messageAllies("<"+player.getName()+"> (ally) "+msg);
-				ClanPlugin.getInstance().log("<"+player.getName()+"> (ally) "+msg);
+				player.getClan().messageAllies("<"+name+"> (ally) "+msg);
+				ClanPlugin.getInstance().log("<"+name+"> (ally) "+msg);
 				return;
 			} else {
 				player.setChatMode(ClanPlayer.CHAT_PUBLIC);
 			}
 		}
-		ClanPlugin.getInstance().messageAll("<"+player.getName()+"> "+msg);
-		ClanPlugin.getInstance().log("<"+player.getName()+"> "+msg);
+		ClanPlugin.getInstance().messageAll("<"+name+"> "+msg);
+		ClanPlugin.getInstance().log("<"+name+"> "+msg);
 		
 	}
 
